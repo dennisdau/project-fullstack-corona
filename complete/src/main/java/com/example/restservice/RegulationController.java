aa8df34e7ee34cf7cfe6d286c22d27dcbc9a1da2
Dennis <d.daumlechner@gmail.com>
2020-08-31 18:00:57 +0200

    added mapping for regulation by state

--- complete/src/main/java/com/example/restservice/RegulationController.java
+++ complete/src/main/java/com/example/restservice/RegulationController.java
@@ -1,6 +1,9 @@
 package com.example.restservice;
 
 import java.util.concurrent.atomic.AtomicLong;
+import java.util.Arrays;
+import java.util.*;
+import java.util.stream.*;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,28 +13,39 @@ import org.springframework.web.bind.annotation.RestController;
 public class RegulationController {
 
     private final AtomicLong counter = new AtomicLong();
-    
+
+
+	List<Regulation> regulationList = Arrays.asList(
+		new Regulation(counter.incrementAndGet(),  "Baden-Württemberg", 10, 20, 5, 8, false, EbuildingTypes.BÜRO, 0.3, 0.1, "none"),
+		new Regulation(counter.incrementAndGet(),  "Bayern", 10, 20, 5, 8, false, EbuildingTypes.BÜRO, 0.3, 0.1, "none"),
+		new Regulation(counter.incrementAndGet(),  "Berlin", 10, 20, 5, 8, false, EbuildingTypes.BÜRO, 0.3, 0.1, "none"),
+		new Regulation(counter.incrementAndGet(),  "Brandenburg", 10, 20, 5, 8, false, EbuildingTypes.BÜRO, 0.3, 0.1, "none"),
+		new Regulation(counter.incrementAndGet(),  "Bremen", 10, 20, 5, 8, false, EbuildingTypes.BÜRO, 0.3, 0.1, "none"),
+		new Regulation(counter.incrementAndGet(),  "Hamburg", 10, 20, 5, 8, false, EbuildingTypes.BÜRO, 0.3, 0.1, "none"),
+		new Regulation(counter.incrementAndGet(),  "Hessen", 10, 20, 5, 8, false, EbuildingTypes.BÜRO, 0.3, 0.1, "none"),
+		new Regulation(counter.incrementAndGet(),  "Mecklenburg-Vorpommern", 10, 20, 5, 8, false, EbuildingTypes.BÜRO, 0.3, 0.1, "none"),
+		new Regulation(counter.incrementAndGet(),  "Niedersachsen", 10, 20, 5, 8, false, EbuildingTypes.BÜRO, 0.3, 0.1, "none"),
+		new Regulation(counter.incrementAndGet(),  "Nordrhein-Westfalen", 10, 20, 5, 8, false, EbuildingTypes.BÜRO, 0.3, 0.1, "none"),
+		new Regulation(counter.incrementAndGet(),  "Rheinland-Pfalz", 10, 20, 5, 8, false, EbuildingTypes.BÜRO, 0.3, 0.1, "none"),
+		new Regulation(counter.incrementAndGet(),  "Saarland", 10, 20, 5, 8, false, EbuildingTypes.BÜRO, 0.3, 0.1, "none"),
+		new Regulation(counter.incrementAndGet(),  "Sachsen", 10, 20, 5, 8, false, EbuildingTypes.BÜRO, 0.3, 0.1, "none"),
+		new Regulation(counter.incrementAndGet(),  "Sachsen-Anhalt", 10, 20, 5, 8, false, EbuildingTypes.BÜRO, 0.3, 0.1, "none"),
+		new Regulation(counter.incrementAndGet(),  "Schleswig-Holstein", 10, 20, 5, 8, false, EbuildingTypes.BÜRO, 0.3, 0.1, "none"),
+		new Regulation(counter.incrementAndGet(),  "Thüringen", 10, 20, 5, 8, false, EbuildingTypes.BÜRO, 0.3, 0.1, "none")
+	);
+	
 
 	@GetMapping("/regulation")
-	public Regulation[] regulation(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return new Regulation[]{
-			new Regulation(counter.incrementAndGet(),  "Baden-Württemberg", 10, 20, 5, 8, false, EbuildingTypes.BÜRO, 0.3, 0.1, "none"),
-			new Regulation(counter.incrementAndGet(),  "Bayern", 10, 20, 5, 8, false, EbuildingTypes.BÜRO, 0.3, 0.1, "none"),
-			new Regulation(counter.incrementAndGet(),  "Berlin", 10, 20, 5, 8, false, EbuildingTypes.BÜRO, 0.3, 0.1, "none"),
-			new Regulation(counter.incrementAndGet(),  "Brandenburg", 10, 20, 5, 8, false, EbuildingTypes.BÜRO, 0.3, 0.1, "none"),
-			new Regulation(counter.incrementAndGet(),  "Bremen", 10, 20, 5, 8, false, EbuildingTypes.BÜRO, 0.3, 0.1, "none"),
-			new Regulation(counter.incrementAndGet(),  "Hamburg", 10, 20, 5, 8, false, EbuildingTypes.BÜRO, 0.3, 0.1, "none"),
-			new Regulation(counter.incrementAndGet(),  "Hessen", 10, 20, 5, 8, false, EbuildingTypes.BÜRO, 0.3, 0.1, "none"),
-			new Regulation(counter.incrementAndGet(),  "Mecklenburg-Vorpommern", 10, 20, 5, 8, false, EbuildingTypes.BÜRO, 0.3, 0.1, "none"),
-			new Regulation(counter.incrementAndGet(),  "Niedersachsen", 10, 20, 5, 8, false, EbuildingTypes.BÜRO, 0.3, 0.1, "none"),
-			new Regulation(counter.incrementAndGet(),  "Nordrhein-Westfalen", 10, 20, 5, 8, false, EbuildingTypes.BÜRO, 0.3, 0.1, "none"),
-			new Regulation(counter.incrementAndGet(),  "Rheinland-Pfalz", 10, 20, 5, 8, false, EbuildingTypes.BÜRO, 0.3, 0.1, "none"),
-			new Regulation(counter.incrementAndGet(),  "Saarland", 10, 20, 5, 8, false, EbuildingTypes.BÜRO, 0.3, 0.1, "none"),
-			new Regulation(counter.incrementAndGet(),  "Sachsen", 10, 20, 5, 8, false, EbuildingTypes.BÜRO, 0.3, 0.1, "none"),
-			new Regulation(counter.incrementAndGet(),  "Sachsen-Anhalt", 10, 20, 5, 8, false, EbuildingTypes.BÜRO, 0.3, 0.1, "none"),
-			new Regulation(counter.incrementAndGet(),  "Schleswig-Holstein", 10, 20, 5, 8, false, EbuildingTypes.BÜRO, 0.3, 0.1, "none"),
-			new Regulation(counter.incrementAndGet(),  "Thüringen", 10, 20, 5, 8, false, EbuildingTypes.BÜRO, 0.3, 0.1, "none")
-		};
+	public List<Regulation> regulation() {
+		return regulationList;
+    }
+    
+    @GetMapping("/regulation1")
+	public List<Regulation> regulationPerState(@RequestParam(value = "stateName") String stateName) {
+	
+
+	return regulationList.stream().filter(regulation -> regulation.getState().equals(stateName)).collect(Collectors.toList());
+        
 	}
 }
 

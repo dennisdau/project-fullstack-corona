2a00d7711775cbfecf9ee945842082588b68212e
Christian <christian.sonntag@hotmail.com>
2020-08-31 15:28:00 +0200

    RegulationController added states

--- complete/src/main/java/com/example/restservice/RegulationController.java
+++ complete/src/main/java/com/example/restservice/RegulationController.java
@@ -13,7 +13,24 @@ public class RegulationController {
 
 	@GetMapping("/regulation")
 	public Regulation[] regulation(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return new Regulation[]{new Regulation(counter.incrementAndGet(),  "Niedersachen", 10, 20, 5, 8, false, "office", 0.3, 0.1, "none")};
+		return new Regulation[]{
+			new Regulation(counter.incrementAndGet(),  "Baden-Württemberg", 10, 20, 5, 8, false, "office", 0.3, 0.1, "none"),
+			new Regulation(counter.incrementAndGet(),  "Bayern", 10, 20, 5, 8, false, "office", 0.3, 0.1, "none"),
+			new Regulation(counter.incrementAndGet(),  "Berlin", 10, 20, 5, 8, false, "office", 0.3, 0.1, "none"),
+			new Regulation(counter.incrementAndGet(),  "Brandenburg", 10, 20, 5, 8, false, "office", 0.3, 0.1, "none"),
+			new Regulation(counter.incrementAndGet(),  "Bremen", 10, 20, 5, 8, false, "office", 0.3, 0.1, "none"),
+			new Regulation(counter.incrementAndGet(),  "Hamburg", 10, 20, 5, 8, false, "office", 0.3, 0.1, "none"),
+			new Regulation(counter.incrementAndGet(),  "Hessen", 10, 20, 5, 8, false, "office", 0.3, 0.1, "none"),
+			new Regulation(counter.incrementAndGet(),  "Mecklenburg-Vorpommern", 10, 20, 5, 8, false, "office", 0.3, 0.1, "none"),
+			new Regulation(counter.incrementAndGet(),  "Niedersachsen", 10, 20, 5, 8, false, "office", 0.3, 0.1, "none"),
+			new Regulation(counter.incrementAndGet(),  "Nordrhein-Westfalen", 10, 20, 5, 8, false, "office", 0.3, 0.1, "none"),
+			new Regulation(counter.incrementAndGet(),  "Rheinland-Pfalz", 10, 20, 5, 8, false, "office", 0.3, 0.1, "none"),
+			new Regulation(counter.incrementAndGet(),  "Saarland", 10, 20, 5, 8, false, "office", 0.3, 0.1, "none"),
+			new Regulation(counter.incrementAndGet(),  "Sachsen", 10, 20, 5, 8, false, "office", 0.3, 0.1, "none"),
+			new Regulation(counter.incrementAndGet(),  "Sachsen-Anhalt", 10, 20, 5, 8, false, "office", 0.3, 0.1, "none"),
+			new Regulation(counter.incrementAndGet(),  "Schleswig-Holstein", 10, 20, 5, 8, false, "office", 0.3, 0.1, "none"),
+			new Regulation(counter.incrementAndGet(),  "Thüringen", 10, 20, 5, 8, false, "office", 0.3, 0.1, "none")
+		};
 	}
 }
 

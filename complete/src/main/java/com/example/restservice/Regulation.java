30dbd3411d9790edf69a80e3284207a0eb037265
Dennis <d.daumlechner@gmail.com>
2020-08-31 15:18:24 +0200

    added Regulation and RegulationController

---
+++ complete/src/main/java/com/example/restservice/Regulation.java
@@ -0,0 +1,84 @@
+package com.example.restservice;
+
+public class Regulation {
+
+    private final long id;
+    private final String state;
+    public final int maxPersonsClosedRooms;
+    public final int maxPersonsOutside;
+    public final int maxHouseholdsClosedrooms;
+    public final int maxHouseholdsOutside;
+    public final boolean maskDuty;
+    public final String closedBuildingtypes;
+    public final double maxPersonsPerSquareMeterClosedRooms;
+    public final double maxPersonsPerSquareMeterOutside;
+    public final String otherRegulations;
+
+
+    public Regulation(long id, String state, int maxPersonsClosedRooms, int maxPersonsOutside, int maxHouseholdsClosedrooms, int maxHouseholdsOutside, boolean maskDuty, String closedBuildingtypes, double maxPersonsPerSquareMeterClosedRooms, double maxPersonsPerSquareMeterOutside, String otherRegulations) {
+        this.id = id;
+        this.state = state;
+        this.maxPersonsClosedRooms = maxPersonsClosedRooms;
+        this.maxPersonsOutside = maxPersonsOutside;
+        this.maxHouseholdsClosedrooms = maxHouseholdsClosedrooms;
+        this.maxHouseholdsOutside = maxHouseholdsOutside;
+        this.maskDuty = maskDuty;
+        this.closedBuildingtypes = closedBuildingtypes;
+        this.maxPersonsPerSquareMeterClosedRooms = maxPersonsPerSquareMeterClosedRooms;
+        this.maxPersonsPerSquareMeterOutside = maxPersonsPerSquareMeterOutside;
+        this.otherRegulations = otherRegulations;
+    }
+
+
+
+    public long getId() {
+        return this.id;
+    }
+
+    public String getState() {
+        return this.state;
+    }
+
+    public int getMaxPersonsClosedRooms() {
+        return this.maxPersonsClosedRooms;
+    }
+
+    public int getMaxPersonsOutside() {
+        return this.maxPersonsOutside;
+    }
+
+    public int getMaxHouseholdsClosedrooms() {
+        return this.maxHouseholdsClosedrooms;
+    }
+
+    public int getMaxHouseholdsOutside() {
+        return this.maxHouseholdsOutside;
+    }
+
+    public boolean getMaskDuty() {
+        return this.maskDuty;
+    }
+
+    public boolean isMaskDuty() {
+        return this.maskDuty;
+    }
+
+    public String getClosedBuildingtypes() {
+        return this.closedBuildingtypes;
+    }
+
+    public double getMaxPersonsPerSquareMeterClosedRooms() {
+        return this.maxPersonsPerSquareMeterClosedRooms;
+    }
+
+    public double getMaxPersonsPerSquareMeterOutside() {
+        return this.maxPersonsPerSquareMeterOutside;
+    }
+
+    public String getOtherRegulations() {
+        return this.otherRegulations;
+    }
+
+
+
+}
--- complete/src/main/java/com/example/restservice/RegulationController.java
+++ complete/src/main/java/com/example/restservice/RegulationController.java
@@ -7,13 +7,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class GreetingController {
+public class RegulationController {
 
-	private static final String template = "Hello, %s!";
 	private final AtomicLong counter = new AtomicLong();
 
-	@GetMapping("/greeting")
-	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+	@GetMapping("/regulation")
+	public Regulation[] regulation(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return new Regulation[]{new Regulation(counter.incrementAndGet(),  "Niedersachen", 10, 20, 5, 8, false, "office", 0.3, 0.1, "none")};
 	}
 }
+
